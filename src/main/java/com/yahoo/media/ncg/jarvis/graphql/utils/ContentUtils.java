c80fb1f33e5bb3a48c65588bd4ffdd53d1b49e06
Hank Ting <hankting@yahooinc.com>
2025-02-17 18:24:51 +0800

    test coverage

--- src/main/java/com/yahoo/media/ncg/jarvis/graphql/utils/ContentUtils.java
+++ src/main/java/com/yahoo/media/ncg/jarvis/graphql/utils/ContentUtils.java
@@ -9,10 +9,6 @@ import java.util.UUID;
 
 @Slf4j
 public class ContentUtils {
-//    public static final ObjectMapper MAPPER = new ObjectMapper()
-//        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-//        .setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
     public static boolean isValidUuid(String id) {
         try {
             if (Strings.isBlank(id)) {
@@ -24,20 +20,4 @@ public class ContentUtils {
             return false;
         }
     }
-
-//    public static String objectToJsonString(Object value) {
-//        if (value != null) {
-//            if (value instanceof List || value instanceof Map) {
-//                try {
-//                    return MAPPER.writeValueAsString(value);
-//                } catch (JsonProcessingException e) {
-//                    log.error("Error converting object to string", e);
-//                }
-//            } else {
-//                return value.toString();
-//            }
-//        }
-//        return null;
-//    }
-
 }
