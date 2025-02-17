c7c157489462b5b2193b151611bc0392cd78e1f0
Hank Ting <hankting@yahooinc.com>
2025-02-17 11:42:10 +0800

    add more test case

--- src/test/java/com/yahoo/media/ncg/jarvis/graphql/utils/ContentUtilTest.java
+++ src/test/java/com/yahoo/media/ncg/jarvis/graphql/utils/ContentUtilTest.java
@@ -1,5 +1,6 @@
 package com.yahoo.media.ncg.jarvis.graphql.utils;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -11,5 +12,10 @@ public class ContentUtilTest {
     @Test
     public void testIsValidUuid() {
         assertTrue(ContentUtils.isValidUuid(UUID.randomUUID().toString()));
+    }
+
+    @Test
+    public void testIsInValidUuid() {
+        assertFalse(ContentUtils.isValidUuid("aa"));
     }
 }
