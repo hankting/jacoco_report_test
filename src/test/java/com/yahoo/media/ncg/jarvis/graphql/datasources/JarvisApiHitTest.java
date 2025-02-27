a48e6f18fb77487828f8acf1a65663cec3aa4459
Hank Ting <hankting@yahooinc.com>
2025-02-27 16:36:08 +0800

    remove existing test

--- src/test/java/com/yahoo/media/ncg/jarvis/graphql/datasources/JarvisApiHitTest.java
+++ src/test/java/com/yahoo/media/ncg/jarvis/graphql/datasources/JarvisApiHitTest.java
@@ -1,18 +1,18 @@
 package com.yahoo.media.ncg.jarvis.graphql.datasources;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//import org.junit.jupiter.api.Test;
 
 public class JarvisApiHitTest {
 
-    @Test
-    void testIsTypeBlogPost() {
-        var jarvisHit = new JarvisApiHit()
-            .setId("dummyId")
-            .setType("ymedia:type=blogpost");
-        assertTrue(jarvisHit.isTypeBlogPost());
-    }
+//    @Test
+//    void testIsTypeBlogPost() {
+//        var jarvisHit = new JarvisApiHit()
+//            .setId("dummyId")
+//            .setType("ymedia:type=blogpost");
+//        assertTrue(jarvisHit.isTypeBlogPost());
+//    }
 
 //    @Test
 //    void testIsTypeTest() {
