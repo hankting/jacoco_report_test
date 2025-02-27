4f776af206d8c1ca1f6cbc3b42a66b45e99a47ff
Hank Ting <hankting@yahooinc.com>
2025-02-27 16:46:09 +0800

    add new test case for unchanged line

--- src/test/java/com/yahoo/media/ncg/jarvis/graphql/datasources/JarvisApiHitTest.java
+++ src/test/java/com/yahoo/media/ncg/jarvis/graphql/datasources/JarvisApiHitTest.java
@@ -1,18 +1,26 @@
 package com.yahoo.media.ncg.jarvis.graphql.datasources;
 
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class JarvisApiHitTest {
 
-//    @Test
-//    void testIsTypeBlogPost() {
-//        var jarvisHit = new JarvisApiHit()
-//            .setId("dummyId")
-//            .setType("ymedia:type=blogpost");
-//        assertTrue(jarvisHit.isTypeBlogPost());
-//    }
+    @Test
+    void testIsTypeBlogPost() {
+        var jarvisHit = new JarvisApiHit()
+            .setId("dummyId")
+            .setType("ymedia:type=blogpost");
+        assertTrue(jarvisHit.isTypeBlogPost());
+    }
+
+    @Test
+    void testIsTypeOffnet() {
+        var jarvisHit = new JarvisApiHit()
+            .setId("dummyId")
+            .setType("ymedia:type=offnet");
+        assertTrue(jarvisHit.isTypeOffnet());
+    }
 
 //    @Test
 //    void testIsTypeTest() {
