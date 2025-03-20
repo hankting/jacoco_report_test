7dfde10820045a5411a30ecdd51f6c20d4cdae01
Hank Ting <hankting@yahooinc.com>
2025-03-20 16:18:41 +0800

    add test case

--- src/main/java/com/yahoo/media/ncg/jarvis/graphql/utils/ContentUtilsV2.java
+++ src/main/java/com/yahoo/media/ncg/jarvis/graphql/utils/ContentUtilsV2.java
@@ -2,6 +2,12 @@ package com.yahoo.media.ncg.jarvis.graphql.utils;
 
 public class ContentUtilsV2 {
     public static boolean isEmpty(String id) {
-        return id == null || id.isEmpty();
+        if (id == null) {
+            return true;
+        }
+        if (id.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 }
